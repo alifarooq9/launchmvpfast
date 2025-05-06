f89f9c798453a251bd6ec06132d16efc8f05bc7b
Ali Farooq <alidotm.me@gmail.com>
2025-05-07 01:08:59 +0500

    feat(www): Added components og image

--- apps/www/src/config/site.ts
+++ apps/www/src/config/site.ts
@@ -5,5 +5,6 @@ export const siteConfig = {
     ogImages: {
         base: 'https://www.launchmvpfast.com/og.png',
         nextjsSaas: 'https://www.launchmvpfast.com/og_nextjs_saas.png',
+        components: 'https://www.launchmvpfast.com/og_components.png',
     },
 } as const
