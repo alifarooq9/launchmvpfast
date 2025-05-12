f33e5f14aa5fa31fef990191611b7037c346c97a
Ali Farooq <alidotm.me@gmail.com>
2025-05-12 19:35:19 +0500

    chore(www): Updated package json

--- apps/www/src/components/ui/form.tsx
+++ apps/www/src/components/ui/form.tsx
@@ -147,6 +147,7 @@ function FormMessage({ className, ...props }: React.ComponentProps<'p'>) {
     return (
         <p
             data-slot="form-message"
+            id={formMessageId}
             className={cn('text-destructive text-sm', className)}
             {...props}
         >
